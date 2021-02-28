0f0ba35d55dd4c2117b635e70e088b3a8feef3f1
Vincent Rastello <vrastello11@gmail.com>
2021-02-28 03:45:11 -0800

    v4

--- js/form.js
+++ js/form.js
@@ -8,6 +8,7 @@ document.getElementById('post_submit').addEventListener('click', function(event)
     req.addEventListener('load',function(){
         if(req.status >= 200 && req.status < 400){
           var response = JSON.parse(req.responseText);
+          //Check Console for confirmation POST is working 
           console.log(response)
           var Thanks = "Thanks for your request, we will get back to you as soon as possible."
           document.getElementById('response').appendChild(document.createTextNode(Thanks))
